b5b20efdb3e00884f2958bd92a94491c042798a8
Agustin <agusfn20@gmail.com>
2016-06-23 00:11:07 -0300

    Corregida barrera de edad de tienda Steam para actualizador precios.

--- global_scripts/php/steam_product_fetch.php
+++ global_scripts/php/steam_product_fetch.php
@@ -212,7 +212,7 @@ function get_htmlpage($url)
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");
 	curl_setopt($ch, CURLOPT_COOKIESESSION, TRUE);
-	curl_setopt($ch, CURLOPT_COOKIE, "birthtime=652950001;");
+	curl_setopt($ch, CURLOPT_COOKIE, "birthtime=652950001;lastagecheckage=1-January-1900;mature_content=1");
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
 	
 	$data = curl_exec($ch);

91577437b3ab664b2a378eaa1d5bd7345d8cb317
Agustin PC <agusfn20@gmail.com>
2016-10-26 22:59:04 -0300

    Prueba

--- index.php
+++ index.php
@@ -233,6 +233,7 @@ $steam_sales_featured_items = 13;
         	
             <div class="main_content">
             	<?php
+				/*
 				if($steam_sales_event) {
 					?>
                     <div class="event_section">
@@ -556,7 +557,9 @@ $steam_sales_featured_items = 13;
     				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
                 </div>
             
-            
+            <?php */
+			
+			?>
             </div><!-- End main content -->
             
         	<?php require_once("global_scripts/php/footer.php"); ?>
